2af6b2150a0a5766476d16e225a373f286a7017a
ertush <erotush77@gmail.com>
2020-08-25 01:46:24 +0300

    Added device.js and gitignore

---
+++ assets/js/device.js
@@ -0,0 +1,69 @@
+
+function detectmob() { 
+    if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ){
+       return true;
+     }
+    else {
+       return false;
+     }
+   }
+
+
+(function(){
+   
+    if (detectmob()) {
+            // Mobile device
+            // Header logo
+
+            $("#logo").append(`<p class="nav-item nav-link">Agrarian</p>`);
+
+            //Alter Main-heading
+            $("#main-heading").empty();
+            $("#main-heading").append(`
+                <a href="#page-top">
+                    <img src="/assets/img/logo-light-green.png" alt="Horizontal-dash-header-Logo-Old" border="0">
+                </a>
+            `);
+            $("#divider").empty();
+        }
+        else
+        {
+            // Desktop/Tablet device
+            // Header logo 
+
+            $("#logo").append(`
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                    <img src="/assets/img/logo-light-green.png" alt="Horizontal-dash-header-Logo-Old" border="0">
+                </a>
+            `);
+
+            // Footer logo
+
+            $("#footer-logo").append(`
+                <a href="#page-top">
+                    <img src="/assets/img/logo-small-cyan-green.png" alt="Horizontal-dash-header-Logo-Old" border="0">
+                </a>
+            `);
+
+            //Backgroud slide Animation
+            $('header').hover(function () {
+                $(this).stop().fadeOut("1000", function () {
+                    $(this).css("background-image", "url('/assets/img/bg-masthead-cabbage.jpg')").fadeIn(1000);
+                });
+            }, function () {
+                $(this).stop().fadeOut("1000", function () {
+                    $(this).css("background-image", "url('/assets/img/bg-masthead-plant-port.jpg')").fadeIn(1000);
+                });
+            });
+        }
+     
+})()
+
+
